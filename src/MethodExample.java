6e8f0c9df74798cf073636be6e7af7f078fc6119
Nika Zhordania <nika.zhordania.1@gmail.com>
2025-03-11 23:22:25 +0400

    init

---
+++ src/MethodExample.java
@@ -0,0 +1,10 @@
+
+public class MethodExample {
+    public static void main(String[] args) {
+        greet();
+    }
+
+    public static void greet() {
+        System.out.println("Hello from a method!");
+    }
+}
